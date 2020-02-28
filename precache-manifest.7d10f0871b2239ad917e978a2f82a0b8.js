b6a028d04e6a7bfb665a059ebbdd6cd885ebfcb6
kevin lautermilch <kevin.laut@outlook.com>
2020-02-28 13:52:31 -0500

    Updates

---
+++ precache-manifest.7d10f0871b2239ad917e978a2f82a0b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900cfdc6a8c4c73be9952a5990faf1a2",
+    "url": "/Sprint-Challenge-Single-Page-Apps/index.html"
+  },
+  {
+    "revision": "30c62bdd425ac50b9a74",
+    "url": "/Sprint-Challenge-Single-Page-Apps/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7af58d98e1a2d277a16d",
+    "url": "/Sprint-Challenge-Single-Page-Apps/static/css/main.53a25bac.chunk.css"
+  },
+  {
+    "revision": "30c62bdd425ac50b9a74",
+    "url": "/Sprint-Challenge-Single-Page-Apps/static/js/2.c55101fe.chunk.js"
+  },
+  {
+    "revision": "99d31cac8f650aa1f1c9574559942819",
+    "url": "/Sprint-Challenge-Single-Page-Apps/static/js/2.c55101fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af58d98e1a2d277a16d",
+    "url": "/Sprint-Challenge-Single-Page-Apps/static/js/main.607b5a20.chunk.js"
+  },
+  {
+    "revision": "78230baca721af8619ad",
+    "url": "/Sprint-Challenge-Single-Page-Apps/static/js/runtime-main.30386581.js"
+  },
+  {
+    "revision": "488d29b979f2e9b7d1aa9a0016f394fa",
+    "url": "/Sprint-Challenge-Single-Page-Apps/static/media/RickandMorty.488d29b9.jpg"
+  }
+]);
